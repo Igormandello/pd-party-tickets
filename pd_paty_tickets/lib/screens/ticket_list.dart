12988b6d859a25ba74c6c3f74d3594380d9f5132
Igormandello <igormandello@gmail.com>
2019-03-05 19:03:48 -0300

    Integrates the ticket store with the ticket list screen

--- pd_paty_tickets/lib/screens/ticket_list.dart
+++ pd_paty_tickets/lib/screens/ticket_list.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_flux/flutter_flux.dart';
+import 'package:intl/intl.dart';
+import '../stores/ticket_store.dart';
 
-class TicketList extends StatelessWidget {
-  final items = List<int>.generate(10000, (i) => i + 1);
+class TicketList extends StatefulWidget {
+
+
+  @override
+  State<StatefulWidget> createState() => new TicketListState();
+}
+
+class TicketListState extends State<TicketList>
+    with StoreWatcherMixin<TicketList> {
+  TicketStore ticketStore;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    ticketStore = listenToStore(ticketStoreToken);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,12 +27,18 @@ class TicketList extends StatelessWidget {
       body: ListView.separated(
         itemBuilder: (context, index) => ListTile(
           leading: Icon(Icons.person),
-          title: Text("Index ${items[index]}")
+          title: Text(
+            ticketStore.tickets[index].id
+          ),
+          trailing: Text(
+            new DateFormat.Hm().format(ticketStore.tickets[index].date),
+            style: Theme.of(context).textTheme.caption,
+          )
         ),
         separatorBuilder: (context, index) => Divider(
           color: Colors.black26,
         ),
-        itemCount: items.length,
+        itemCount: ticketStore.tickets.length,
       )
     );
   }
