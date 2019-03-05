632b69db5857012c8a5e7805db2832e90408750b
Igormandello <igormandello@gmail.com>
2019-03-05 19:03:48 -0300

    Adds the flutter_flux library and creates a ticket store
    - Creates only the addTicket action

---
+++ pd_paty_tickets/lib/stores/ticket_store.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_flux/flutter_flux.dart';
+
+class Ticket {
+  Ticket({ this.id, this.date });
+  final String id;
+  final DateTime date;
+}
+
+class TicketStore extends Store {
+  TicketStore() {
+    triggerOnAction(addTicket, (Ticket ticket) {
+      this._tickets.add(ticket);
+      this._tickets.sort((a, b) => a.id.compareTo(b.id));
+    });
+  }
+
+  final List<Ticket> _tickets = <Ticket>[];
+  List<Ticket> get tickets =>
+      new List<Ticket>.unmodifiable(_tickets);
+}
+
+final StoreToken ticketStoreToken = new StoreToken(new TicketStore());
+
+final Action<Ticket> addTicket = new Action<Ticket>();
