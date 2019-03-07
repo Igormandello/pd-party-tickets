b2f07ec0bf7a650a79414a6e5685fb4ba264ddac
Igormandello <igormandello@gmail.com>
2019-03-06 23:21:32 -0300

    Adds a dev_main to register environment variables
    - Creates a dev_env.json asset, when the dev_main is started, the configurations of this file are put in the GlobalConfiguration object

--- pd_paty_tickets/lib/app.dart
+++ pd_paty_tickets/lib/app.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:pd_paty_tickets/screens/home.dart';
+import 'package:pd_paty_tickets/screens/login.dart';
 
 class App extends StatelessWidget {
   App({ Key key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Home(),
+    return MaterialApp(
+      title: 'PD Party Tickets',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: Home(),
     );
-  }
+}
 }
